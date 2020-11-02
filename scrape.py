d23a5cca789757e11f204ed8203193e4b624badd
Your Name <andrey.andonov@yahoo.com>
2020-11-02 10:50:27 +0200

    Update

--- scrape.py
+++ scrape.py
@@ -1,12 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
+import requests  # module which allows us to grab html files and download it
+from bs4 import BeautifulSoup  # allows us to scrape websites using the html and cleaning the data
 import pprint
 
+# create new response getting the html data from a website
 res = requests.get('https://news.ycombinator.com/news')
 res2 = requests.get('https://news.ycombinator.com/news?p=2')
+
+# modifying the string into html data
 soup = BeautifulSoup(res.text, 'html.parser')
 soup2 = BeautifulSoup(res2.text, 'html.parser')
 
+# use CSS selector to select nested elements
+# g
 links = soup.select('.storylink')
 subtext = soup.select('.subtext')
 links2 = soup2.select('.storylink')
@@ -17,21 +22,21 @@ mega_subtext = subtext + subtext2
 
 
 def sort_stories_by_votes(hnlist):
-  return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
 
 def create_custom_hn(links, subtext):
-  hn = []
-  for idx, item in enumerate(links):
-    title = item.getText()
-    href = item.get('href', None)
-    vote = subtext[idx].select('.score')
-    if len(vote):
-      points = int(vote[0].getText().replace(' points', '').replace(' point', ''))
-      print(points)
-      if points > 120:
-        hn.append({'title': title, 'link': href, 'votes': points})
-  return sort_stories_by_votes(hn)
- 
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', '').replace(' point', ''))
+            print(points)
+            if points > 120:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
+
 
 pprint.pprint(create_custom_hn(mega_links, mega_subtext))
